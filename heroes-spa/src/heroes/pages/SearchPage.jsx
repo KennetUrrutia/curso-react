d5b1a31612c255c6ffa5d5b5024fb93eff43288d
Kenneth De Leon <Kenneth@somosmangoverde.com>
2023-01-25 18:36:57 -0600

    arreglos para busqueda de heroes

--- heroes-spa/src/heroes/pages/SearchPage.jsx
+++ heroes-spa/src/heroes/pages/SearchPage.jsx
@@ -11,15 +11,16 @@ export const Search = () => {
   const { q = '' } = queryString.parse(location.search)
   const heroes = getHerosByName(q)
 
+  const showSearch = (q.length === 0) // Es una condicion que regresa un valor bool, ej. q.length === 0 ? true : false
+  const showError = (q.length > 0) && (heroes.length === 0) // Es una condicion que regresa un valor bool, ej. q.length === 0 ? true : false
+
   const { searchText, handleInputChange } = useForm({
     searchText: ''
   })
 
   const handleSubmit = (e) => {
     e.preventDefault()
-
-    if (searchText.trim().length <= 1) return
-
+    // if (searchText.trim().length <= 1) return
     navigate(`?q=${searchText}`)
   }
 
@@ -51,12 +52,19 @@ export const Search = () => {
         <div className='col-7'>
           <h4>Results</h4>
           <hr />
-          <div className='alert alert-primary'>
+          <div
+            className='alert alert-primary animate__animated animate__fadeIn'
+            style={{ display: showSearch ? '' : 'none' }}
+          >
             Search a Hero
           </div>
-          <div className='alert alert-danger'>
-            No hero with <b>ABC</b>
+          <div
+            className='alert alert-danger animate__animated animate__fadeIn'
+            style={{ display: showError ? '' : 'none' }}
+          >
+            No hero with <b>{q}</b>
           </div>
+
           {heroes.map(hero => (
             <HeroCard key={hero.id} {...hero} />
           ))}
